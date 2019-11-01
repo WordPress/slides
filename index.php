9b76442d4bb3bfa29955c284e9736a8ef33541d8
iseulde <wp@iseulde.com>
2019-11-01 12:15:25 +0100

    0.0.28

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Slide
  * Plugin URI:  https://wordpress.org/plugins/slide/
  * Description: Allows you to create presentations with the block editor.
- * Version:     0.0.27
+ * Version:     0.0.28
  * Author:      Ella van Durpe
  * Author URI:  https://ellavandurpe.com
  * Text Domain: slide
