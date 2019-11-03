1aa7ec0197a8952c9b8e1f9453572879d7426629
pedro-mendonca <ped.gaspar@gmail.com>
2019-11-03 17:52:40 +0000

    Add missing gettext

--- speaker.php
+++ speaker.php
@@ -18,32 +18,32 @@
 	<body>
         <div id="slide-container">
             <div id="current-slide-container"><div id="current-slide"></div></div>
-            <div id="upcoming-slide-container"><div id="upcoming-slide"><span class="overlay-element label">Upcoming</span></div></div>
+            <div id="upcoming-slide-container"><div id="upcoming-slide"><span class="overlay-element label"><?php esc_html_e( 'Upcoming', 'slide' ); ?></span></div></div>
             <div class="speaker-controls-time">
                 <div>
-                    <button id="prev">Previous</button>
-                    <button id="next">Next</button>
+                    <button id="prev"><?php esc_html_e( 'Previous', 'slide' ); ?></button>
+                    <button id="next"><?php esc_html_e( 'Next', 'slide' ); ?></button>
                 </div>
-                <h4 class="label">Slide</h4>
+                <h4 class="label"><?php esc_html_e( 'Slide', 'slide' ); ?></h4>
                 <div class="slide-count">
                     <span class="slide-current-number">&hellip;</span> of <span class="slide-total-number">&hellip;</span>
                 </div>
-                <h4 class="label">Time</h4>
+                <h4 class="label"><?php esc_html_e( 'Time', 'slide' ); ?></h4>
                 <div class="clock">
                     <span class="clock-value">0:00 AM</span>
                 </div>
-                <h4 class="label"><span class="reset-button">Click to Reset</span></h4>
+                <h4 class="label"><span class="reset-button"><?php esc_html_e( 'Click to Reset', 'slide' ); ?></span></h4>
                 <div class="timer">
                     <span class="hours-value">00</span><span class="minutes-value">:00</span><span class="seconds-value">:00</span>
                 </div>
                 <div>
-                    <button id="editor" data-href="<?php echo get_edit_post_link() ?>">Presentiation editor (public)</button>
-                    <div style="font-size: 14px;margin-top:5px;">This will exit speaker view. To enter speaker view again, use the browser back button, then click the "Speaker View" button in the admin bar.</div>
+                    <button id="editor" data-href="<?php echo get_edit_post_link() ?>"><?php esc_html_e( 'Presentation editor (public)', 'slide' ); ?></button>
+                    <div style="font-size: 14px;margin-top:5px;"><?php esc_html_e( 'This will exit speaker view. To enter speaker view again, use the browser back button, then click the "Speaker View" button in the admin bar.', 'slide' ); ?></div>
                 </div>
             </div>
         </div>
         <div class="speaker-controls-notes">
-            <h4 class="label">Notes</h4>
+            <h4 class="label"><?php esc_html_e( 'Notes', 'slide' ); ?></h4>
             <div class="value"></div>
         </div>
         <?php wp_footer(); ?>
