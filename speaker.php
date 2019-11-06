4db5e29f3bd154149b2f1717a06657c2f21aea5e
Jeremy Felt <jeremy.felt@gmail.com>
2019-11-05 18:01:47 -0800

    Correct spelling in presentation editor link

--- speaker.php
+++ speaker.php
@@ -37,7 +37,7 @@
                     <span class="hours-value">00</span><span class="minutes-value">:00</span><span class="seconds-value">:00</span>
                 </div>
                 <div>
-                    <button id="editor" data-href="<?php echo get_edit_post_link() ?>">Presentiation editor (public)</button>
+                    <button id="editor" data-href="<?php echo get_edit_post_link() ?>">Presentation editor (public)</button>
                     <div style="font-size: 14px;margin-top:5px;">This will exit speaker view. To enter speaker view again, use the browser back button, then click the "Speaker View" button in the admin bar.</div>
                 </div>
             </div>
