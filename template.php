6e4db6dc9811e1939338acd2530e44aca36ebd00
iseulde <wp@iseulde.com>
2019-11-06 13:17:34 +0100

    Group dynamic styles

--- template.php
+++ template.php
@@ -61,47 +61,6 @@
 			background: #000;
 		}
 
-		.reveal {
-			color: <?php echo get_post_meta( get_the_ID(), 'presentation-color', true ) ?: '#000'; ?>;
-			font-size: <?php echo get_post_meta( get_the_ID(), 'presentation-font-size', true ) ?: '42'; ?>px;
-			font-family: <?php echo get_post_meta( get_the_ID(), 'presentation-font-family', true ) ?: 'Helvetica, sans-serif'; ?>;
-		}
-
-		.reveal h1,
-		.reveal h2,
-		.reveal h3,
-		.reveal h4,
-		.reveal h5,
-		.reveal h6 {
-			font-family: <?php echo get_post_meta( get_the_ID(), 'presentation-font-family-heading', true ) ?: 'inherit'; ?>;
-			font-weight: <?php echo get_post_meta( get_the_ID(), 'presentation-font-weight-heading', true ) ?: 'inherit'; ?>;
-		}
-
-		/* Extra specificity to override reveal background. */
-		.reveal .slide-background {
-			background-color: <?php echo get_post_meta( get_the_ID(), 'presentation-background-color', true ) ?: '#fff'; ?>;
-			background-image: <?php echo get_post_meta( get_the_ID(), 'presentation-background-gradient', true ) ?: 'none'; ?>;
-		}
-
-		/* If a background color is set, disable the global gradient. */
-		.reveal .slide-background[style*="background-color"] {
-			background-image: none;
-		}
-
-		.reveal .slide-background .slide-background-content {
-			background-image: url("<?php echo get_post_meta( get_the_ID(), 'presentation-background-url', true ) ?: 'none'; ?>");
-			background-position: <?php echo get_post_meta( get_the_ID(), 'presentation-background-position', true ) ?: '50% 50%'; ?>;
-			opacity: <?php echo (int) get_post_meta( get_the_ID(), 'presentation-background-opacity', true ) / 100 ?: '1'; ?>;
-		}
-
-		.reveal .slides section.wp-block-slide-slide {
-			top: auto !important;
-			padding-top: <?php echo get_post_meta( get_the_ID(), 'presentation-vertical-padding', true ) ?: '0.2em'; ?> !important;
-			padding-bottom: <?php echo get_post_meta( get_the_ID(), 'presentation-vertical-padding', true ) ?: '0.2em'; ?> !important;
-			padding-left: <?php echo get_post_meta( get_the_ID(), 'presentation-horizontal-padding', true ) ?: '0.2em'; ?> !important;
-			padding-right: <?php echo get_post_meta( get_the_ID(), 'presentation-horizontal-padding', true ) ?: '0.2em'; ?> !important;
-		}
-
 		.reveal .slides {
 			text-align: inherit;
 			justify-content: center;
@@ -174,22 +133,65 @@
 			max-height: 100vh;
 		}
 
+		.reveal[data-background-transition="none"] .slide-background {
+			transition: none;
+		}
+
+		/* Dynamic */
+
+		.reveal {
+			color: <?php echo sanitize_hex_color( get_post_meta( get_the_ID(), 'presentation-color', true ) ) ?: '#000'; ?>;
+			font-size: <?php echo (int) get_post_meta( get_the_ID(), 'presentation-font-size', true ) ?: 42; ?>px;
+			font-family: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-font-family', true ) ) ?: 'Helvetica, sans-serif'; ?>;
+		}
+
+		.reveal h1,
+		.reveal h2,
+		.reveal h3,
+		.reveal h4,
+		.reveal h5,
+		.reveal h6 {
+			font-family: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-font-family-heading', true ) ) ?: 'inherit'; ?>;
+			font-weight: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-font-weight-heading', true ) ) ?: 'inherit'; ?>;
+		}
+
+		/* Extra specificity to override reveal background. */
+		.reveal .slide-background {
+			background-color: <?php echo sanitize_hex_color( get_post_meta( get_the_ID(), 'presentation-background-color', true ) ) ?: '#fff'; ?>;
+			background-image: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-background-gradient', true ) ) ?: 'none'; ?>;
+		}
+
+		/* If a background color is set, disable the global gradient. */
+		.reveal .slide-background[style*="background-color"] {
+			background-image: none;
+		}
+
+		.reveal .slide-background .slide-background-content {
+			background-image: url("<?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-background-url', true ) ) ?: 'none'; ?>");
+			background-position: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-background-position', true ) ) ?: '50% 50%'; ?>;
+			opacity: <?php echo (int) get_post_meta( get_the_ID(), 'presentation-background-opacity', true ) / 100 ?: 1; ?>;
+		}
+
+		.reveal .slides section.wp-block-slide-slide {
+			top: auto !important;
+			padding-top: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-vertical-padding', true ) ) ?: '0.2em'; ?> !important;
+			padding-bottom: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-vertical-padding', true ) ) ?: '0.2em'; ?> !important;
+			padding-left: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-horizontal-padding', true ) ) ?: '0.2em'; ?> !important;
+			padding-right: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-horizontal-padding', true ) ) ?: '0.2em'; ?> !important;
+		}
+
 		.presentation-contain .alignfull {
-			margin: 0 calc( -50px - <?php echo get_post_meta( get_the_ID(), 'presentation-horizontal-padding', true ) ?: '0.2em'; ?> );
+			margin: 0 calc( -50px - <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-horizontal-padding', true ) ) ?: '0.2em'; ?> );
 			max-width: none;
 		}
 
 		.reveal .slides > section,
 		.reveal .slides > section > section {
-			padding: <?php echo get_post_meta( get_the_ID(), 'presentation-vertical-padding', true ) ?: '0.2em'; ?> 0;
-		}
-
-		.reveal[data-background-transition="none"] .slide-background {
-			transition: none;
+			padding: <?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-vertical-padding', true ) ) ?: '0.2em'; ?> 0;
 		}
 	</style>
 	<style>
-		<?php echo get_post_meta( get_the_ID(), 'presentation-css', true ); ?>
+		<?php echo esc_html( get_post_meta( get_the_ID(), 'presentation-css', true ) ); ?>
 	</style>
 </head>
 <body>
